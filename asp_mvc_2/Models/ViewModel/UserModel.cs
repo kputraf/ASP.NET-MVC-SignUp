e2b984c2c120be7eebf1d3898c1fa17d85999ec9
Krisna Putra Fardiansyah <contact.krisnapf@gmail.com>
2017-02-13 08:16:26 +0700

    modul 3

--- asp_mvc_2/Models/ViewModel/UserModel.cs
+++ asp_mvc_2/Models/ViewModel/UserModel.cs
@@ -1,5 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
+<<<<<<< HEAD
+using System.Collections.Generic;
+=======
 
+>>>>>>> cdc5256a9bbdb23d8f5ea7193967ec209c89d6e7
 namespace asp_mvc_2.Models.ViewModel
 {
     public class UserSignUpView
@@ -35,4 +39,56 @@ namespace asp_mvc_2.Models.ViewModel
         [Display(Name = "Password")]
         public string Password { get; set; }
     }
+<<<<<<< HEAD
+    public class UserProfileView
+    {
+        [Key]
+        public int SYSUserID { get; set; }
+        public int LOOKUPRoleID { get; set; }
+        public string RoleName { get; set; }
+        public bool? IsRoleActive { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Login ID")]
+        public string LoginName { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+    }
+    public class LOOKUPAvailableRole
+    {
+        [Key]
+        public int LOOKUPRoleID { get; set; }
+        public string RoleName { get; set; }
+        public string RoleDescription { get; set; }
+    }
+    public class Gender
+    {
+        public string Text { get; set; }
+        public string Value { get; set; }
+    }
+    public class UserRoles
+    {
+        public int? SelectedRoleID { get; set; }
+        public IEnumerable<LOOKUPAvailableRole> UserRoleList { get; set; }
+    }
+    public class UserGender
+    {
+        public string SelectedGender { get; set; }
+        public IEnumerable<Gender> Gender { get; set; }
+    }
+    public class UserDataView
+    {
+        public IEnumerable<UserProfileView> UserProfile { get; set; }
+        public UserRoles UserRoles { get; set; }
+        public UserGender UserGender { get; set; }
+    }
+=======
+>>>>>>> cdc5256a9bbdb23d8f5ea7193967ec209c89d6e7
 }
